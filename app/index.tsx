1c18cae61d60ebf42e7e04e36b19b1b93997e462
JoaoVictor-Noschang <joaovictornoschangsilva601@gmail.com>
2025-06-20 11:53:46 -0400

    dados sendo incluidos no db

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
-import { StyleSheet, View, Text, Image, Alert, TouchableOpacity, ScrollView  } from 'react-native';
+import { StyleSheet, View, Text, Image, Alert, TouchableOpacity, ScrollView, TextInput, Button } from 'react-native';
+import { useEffect, useState } from 'react';
+import * as SQLite from 'expo-sqlite';
 
-import Note from '@/components/Note';
+
+//import Note from '@/components/Note';
 
 export default function AssetExample() {
 
@@ -8,6 +11,51 @@ export default function AssetExample() {
     Alert.alert('Você clicou para cadastrar uma nota!');
     // Aqui você pode abrir um modal, navegar para outra tela, etc.
   };
+
+  const [listNotes, setListNotes] = useState([]);
+  const [textInput, setTextInput] = useState("");
+
+  const addNewNote = () => {
+    let newArray = listNotes;
+    newArray.push(textInput);
+    setListNotes(newArray);
+  };
+
+  type Nota = {
+        id: number;
+        value: string;
+        intValue: number;
+      };
+
+  useEffect(() => {
+    async function setup() {
+
+      //Iniciando o database
+      const db = await SQLite.openDatabaseAsync('databaseNotes');
+
+      //Criando a tabela - notas
+      //await db.execAsync(`
+      //  PRAGMA journal_mode = WAL;
+      //  CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY NOT NULL, value TEXT NOT NULL, intValue INTEGER);
+      //  INSERT INTO notas (value, intValue) VALUES ('Nota01', 123);
+      //  INSERT INTO notas (value, intValue) VALUES ('Nota02', 456);
+      //  INSERT INTO notas (value, intValue) VALUES ('Nota03', 789);
+      //`);
+
+      //pegando todos os registros
+      const allRows = (await db.getAllAsync('SELECT * FROM notas')) as Nota[];
+      let newArray = [];
+      for (const row of allRows) {
+        console.log(row.id, row.value, row.intValue);
+        newArray.push(row.value);
+      }
+      setListNotes(newArray);
+
+    }
+
+    setup();
+
+  }, [])
 
   return (
     <View style={styles.tela}>
@@ -18,7 +66,14 @@ export default function AssetExample() {
           style={styles.perfil}
         />
       </View>
-      <TouchableOpacity style={styles.cadastrar} onPress={handlePressInput}>
+
+      <Text style={styles.place}>Título da Nota</Text>
+      <TextInput
+        style={{ borderColor: '#000000', borderWidth: 1 }}
+        onChangeText={setTextInput}
+      ></TextInput>
+
+      <TouchableOpacity style={styles.cadastrar} onPress={() => addNewNote()}>
         <Image
           source={{
             uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
@@ -27,17 +82,20 @@ export default function AssetExample() {
         />
         <Text style={styles.place}>Cadastrar Nota</Text>
       </TouchableOpacity>
+
+
       <Text style={styles.subtitle}>Notas Registradas</Text>
       <ScrollView>
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
+
+        {
+          listNotes.map((item, index) => {
+            return (<View key={index}>
+              <Text>{item}</Text>
+            </View>)
+          })
+        }
+
+
       </ScrollView>
     </View>
   );
